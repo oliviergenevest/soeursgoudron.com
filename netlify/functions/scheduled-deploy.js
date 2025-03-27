c55a08a24340103ef68f485c9df6c8cb8fd645aa
oliviergenevest <olivier.genevest@gmail.com>
2025-03-27 16:08:30 +0100

    add netlify function to rebuild daily

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import { schedule } from '@netlify/functions'
 
 
-const BUILD_HOOK = 'https://api.netlify.com/build_hooks/66891acae53931839d508f09'
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/67e568c15efcdb48ef599b04'
 
 // Schedules the handler function to run “At 00:00.” (2h du matin en france)
 const handler = schedule('0 0 * * *', async () => {
